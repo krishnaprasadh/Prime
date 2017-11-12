df85fd589c775d0effd2487248aacc8fbce9ddeb
Krishnaprasadh <rkpamb@gmail.com>
2017-11-12 14:37:37 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void prime(int num);
+int primeno,compositeno;
+int main()
+{
+int x;
+for(x=1;x<=100;x++)
+{
+prime(x);
+}
+printf(" Totalprime=%d, Totalcomposite=%d\n",primeno,compositeno);
+}
+
+void prime(int num)
+{
+int p,q,i,prime,composite;
+for(int i=1;i<=num;i++)
+{
+if(num%i==0)
+{p++;}
+else
+{q++;}
+}
+if(p==2)
+{
+printf(" %d-prime\n",num);
+primeno++;
+}
+else
+{
+printf(" %d-composite\n",num);
+compositeno++;
+}
+p=0;
+}
